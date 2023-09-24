86969dd609b82fc60588deb85cad3a8c76bdf89e
Gabriel Caetano Farias <gabrielcfarias77@gmail.com>
2023-09-24 17:59:49 -0300

    Novo Model e Atualizações no Painel do Admin
    +model Perfil adicionado (recebe nome, descrição, data e hora de criação e atualização)
    
    +model também pode ser gerido no Painel de Admin
    
    +criação de super user para login no painel

--- cesarconnect/ccapp/models.py
+++ cesarconnect/ccapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+# class Login(models.Model):
+#     #email = 
+#     nome = models.CharField(max_length=200)
+#     #senha = 
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField(null=True, blank=True)
+    criado = models.DateTimeField(auto_now_add=True)
+    atualizado = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nome
+
