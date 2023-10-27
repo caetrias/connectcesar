676ee901e9a793110c0ef8ac96eaa1d49f49be20
Emanuel Eduardo <112293589+couboii@users.noreply.github.com>
2023-10-27 08:04:37 -0300

    adição de tempo de espera na automação

--- cesarconnect/ccapp/tests.py
+++ cesarconnect/ccapp/tests.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By  # Importe a classe By
 from selenium.webdriver.common.keys import Keys  # Importe a classe Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # Inicializa o driver do Chrome
 driver = webdriver.Chrome()
@@ -11,11 +12,14 @@ driver = webdriver.Chrome()
 # Navega para a página desejada
 driver.get('http://127.0.0.1:8000/')
 wait = WebDriverWait(driver, 10)
+time.sleep(3)
 #input_element = wait.until(EC.presence_of_element_located((By.ID, 'exampleInputEmail1')))
 input_element = driver.find_element(By.ID, 'exampleInputEmail1')
 input_element.click()
 input_element.send_keys('edu@gmail.com' + Keys.RETURN)
+time.sleep(3)
 input_element2 = wait.until(EC.presence_of_element_located((By.ID, 'exampleInputPassword1')))
 input_element2.click()
 input_element2.send_keys('1234' + Keys.RETURN)
+time.sleep(3)
 driver.quit()
